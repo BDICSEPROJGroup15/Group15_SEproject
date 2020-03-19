53dc656b861f310a3316c63e20cdcd01bbd49c22
YaoxinyuFrank <yaoxinyu199823@163.com>
2020-03-19 19:00:26 +0800

    no message

--- VeterinaryHospital/Backend/src/models.py
+++ VeterinaryHospital/Backend/src/models.py
@@ -32,3 +32,10 @@ class Pet(db.Model):
 
 # class Reservation(db.Model):
 
+class Message(db.Model):
+    id=db.Column(db.Integer,primary_key=True,autoincrement=True)
+    content=db.Column(db.String(128),index=True)
+    user_id=db.Column(db.Integer,db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Message {}>'.format(self.content)

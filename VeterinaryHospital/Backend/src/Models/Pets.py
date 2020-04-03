9a050398f3bf1d23cc5f5b6128dda1f725b8588b
Arno Li <guiwecgdiu@163.com>
2020-04-03 20:37:21 +0800

    // Pet and Reservation // Haven't tested

--- VeterinaryHospital/Backend/src/Models/Pets.py
+++ VeterinaryHospital/Backend/src/Models/Pets.py
@@ -12,12 +12,49 @@ class Pet(db.Model):
     pettype=db.Column(db.String(100))
     petowner=db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def create_pet(self, name, age, type, owner):
+    def __init__(self):
+        self.cache={}
+
+    @staticmethod
+    def add_pet(name, age, type, owner=None):
         pet = Pet(petname=name, petage=age, pettype=type, petowner=owner)
         db.session.add(pet)
         db.session.commit()
         return pet
 
-    def __repr__(self):
-        return '{}'.format(self.petname)
+    @staticmethod
+    def remove_pet(id):
+        pet= Pet.get_pet(id)
+        db.session.delete(pet)
+        db.session.commit()
+        return pet
+
+        # read method
+
+    @staticmethod
+    def read_all(limit=None, order_by=None):
+        pets = []
+        query = Pet.query
+        # if limit is not None:
+        #     query=query.limit(limit)
+        # if order_by is not None:
+        #     query=query.order_by()
+        pets = query.all()
+        return pets
+
+    @staticmethod
+    def get_pet(id=None):
+        pet = None
+        if id is None:
+            pet=Pet.query.all().first()
+        else:
+            pet=Pet.query.filter_by(id = id).first()
+        return pet
+
+
+
+
+
+    # def __repr__(self):
+    #     return '{}'.format(self.petname)
 
--- VeterinaryHospital/Backend/src/Models/Reservations.py
+++ VeterinaryHospital/Backend/src/Models/Reservations.py
@@ -1,5 +1,5 @@
 from src import db
-
+from datetime import datetime
 
 class Reservation(db.Model):
     __tablename__='reservation'
@@ -9,7 +9,51 @@ class Reservation(db.Model):
     type = db.Column(db.Enum('emmergency','standard'))
     place = db.Column(db.Enum('Beijing','Shanghai','Chengdu'))
     state = db.Column(db.Boolean)
-    time = db.Column(db.Time)
+    time = db.Column(db.DateTime,default=datetime.now(),nullable=False)
+
+    @staticmethod
+    def add_res(staff, pet, type, place, state):
+        print("Template: (staff,pet, 'emergency', 'Beijing',true)")
+        if type in ['emmergency','stamdard'] and place in ['Beijing','Shanghai','Chengdu'] and isinstance(type,bool):
+            reservation = Reservation(id = staff.id, pet=pet.id, type=type, place=place, state=state)
+            db.session.add(reservation)
+            db.session.commit()
+            return reservation
+        else:
+            return 'Invalid'
+
+
+
+    @staticmethod
+    def remove_res(id):
+        res= Reservation.get_res(id)
+        db.session.delete(res)
+        db.session.commit()
+        return res
+
+        # read method
+
+    @staticmethod
+    def read_all(limit=None, order_by=None):
+        query = Reservation.query
+        # if limit is not None:
+        #     query=query.limit(limit)
+        # if order_by is not None:
+        #     query=query.order_by()
+        ress = query.all()
+        return ress
+
+    @staticmethod
+    def get_res(id=None):
+        res = None
+        if id is None:
+            res=Reservation.query.all().first()
+        else:
+            res=Reservation.query.filter_by(id = id).first()
+        return res
+
+
+
 
     def __repr__(self):
         return '{}'.format(self.id)

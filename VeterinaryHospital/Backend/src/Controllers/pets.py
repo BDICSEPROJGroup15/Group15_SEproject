f3702dfc78843c0b11af1be3d9327659adae817f
17205981 <xiulin.wu@ucdconnect.ie>
2020-04-25 11:16:09 +0800

    add

--- VeterinaryHospital/Backend/src/Controllers/pets.py
+++ VeterinaryHospital/Backend/src/Controllers/pets.py
@@ -17,13 +17,39 @@ pets = Blueprint('pets', __name__, template_folder="/template/pets",
                         static_folder='static/uploaded_PH', url_prefix='/pets')
 
 @pets.route('/')
-@pets.route('/index')
+@pets.route('/index',methods=['GET','POST'])
 def index():
-    return render_template('pets/myPets.html', title='myPets')
+    form = PetForm()
+    if not session.get("USERNAME") is None:
+        user_in_db = User.query.filter(User.username == session.get("USERNAME")).first()
+        if form.validate_on_submit():
+            pet_in_db = Pet.query.filter(Pet.petname == form.petname.data).first()
+            ph_dir = Config.PH_UPLOAD_DIR
+            file_obj = form.petimage.data
+            ph_filename = form.petname.data + '_PPH.jpg'
+            file_obj.save(os.path.join(ph_dir, ph_filename))
+            flash('Photo uploaded and saved')
+            if not pet_in_db:
+                # if no profile exists, add a new object
+                pet = Pet(petname=form.petname.data, petimage=ph_filename, petage=form.petage.data,
+                          pettype=form.pettype.data,user=user_in_db)
+                db.session.add(pet)
+            else:
+                pet_in_db.petimage = ph_filename
+                pet_in_db.petage = form.petage.data
+                pet_in_db.pettype = form.pettype.data
+            # remember to commit
+            db.session.commit()
+            return redirect(url_for('pets.myPets'))
+        else:
+            return render_template('pets/petcenter.html', title='petcenter', form=form)
+    else:
+        flash("User needs to either login or sign up first")
+        return redirect('/login')
 	
 @pets.route('/treatPet', methods=['GET', 'POST'])
 def addPet():
-    form = PetForm()
+    form=PetForm()
     if not session.get("USERNAME") is None:
         user_in_db = User.query.filter(User.username == session.get("USERNAME")).first()
         if form.validate_on_submit():
@@ -59,8 +85,3 @@ def myPets():
         return render_template('pets/myPets.html', title='myPets', pets=pets)
     else:
         return render_template('pets/myPets.html', title='myPets', pets=None)
-		
-@pets.route('/pets/petcenter',methods=['GET','POST'])
-def petCenter():
-	form=PetForm()
-	return render_template('pets/petcenter.html', title='petcenter', pets=None,form=form)

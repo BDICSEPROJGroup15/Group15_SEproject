129ee03fb8a9981f06bb00647ead171f5d612210
Arno Li <guiwecgdiu@163.com>
2020-04-26 21:04:51 +0800

    重构

--- VeterinaryHospital/Backend/src/__init__.py
+++ VeterinaryHospital/Backend/src/__init__.py
@@ -1,41 +1,66 @@
-from email.mime import application
-
-from flask import Flask
-
-
-from flask_mail import Mail
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-
-
-import sys
-sys.path.append('..')
-from config import Config
-
-
-app = Flask(__name__)
-app.config.from_object(Config)
-
-
-base = SQLAlchemy(app)
-
-db = SQLAlchemy(app)
-mail = Mail(app)
+import os
+from src.setting import config
+from flask import Flask,render_template,Blueprint
+# from src.blueprints.auth import auth
+# from src.blueprints.blog import blog
+# from src.blueprints.main import main
+# from src.blueprints.admin import admin
+from src.extension import mail,db,moment,bootstrap
 
 
 
 
-# Scss(app,static_dir='static/style/scss2css', asset_dir='assets')
+def create_app(config_name=None):
+    if config_name is None:
+        config_name=os.getenv('FLASK_CONFIG','development')
+
+    app=Flask(__name__, template_folder='templates')
+    app.config.from_object(config[config_name])
+    # enable CORS
+    CORS(app, resources={r'/*': {'origins': '*'}})
+
+    # register_blueprint(app)
+    register_error(app)
+    register_externsion(app)
+    register_shell_context(app)
+    register_logging(app)
+    # register_commands(app)
+    return app
+
 #
+# def register_blueprint(app):
+#     app.register_blueprint(blueprint=auth)
+#     app.register_blueprint(blueprint=blog)
+#     app.register_blueprint(blueprint=main)
+#     app.register_blueprint(blueprint=admin, url_prefix='/admin')
 
 
-def create_app(test_config=None):
-    mail.init_app(application)
-    db.init_app(application)
+def register_logging(app):
+    pass
+
+def register_externsion(app):
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+    bootstrap.init_app(app)
+
+def register_shell_context(app):
+    @app.shell_context_processor
+    def make_shell_context():
+        return dict(db=db)
+
+def register_error(app):
+    @app.errorhandler(400)
+    def bad_request(e):
+        return render_template('error/400.html'), 400
+
+# def register_commands(app):
+#     # .......
+#     return ""
+app=create_app(None)
 
 
-# enable CORS
-CORS(app, resources={r'/*': {'origins': '*'}})
 
 # User Module
 from src.Blueprint import UserController
---
+++ VeterinaryHospital/Backend/src/setting.py
@@ -0,0 +1,38 @@
+import os
+
+basedir=os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+class BaseConfig(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    CV_UPLOAD_DIR = os.path.join(basedir, 'uploaded_CV')
+    PH_UPLOAD_DIR = os.path.join(basedir, 'src', 'static', 'uploaded_PH')
+
+    MAIL_SERVER = 'smtp.163.com'
+    MAIL_USERNAME = 'wsdsgbxd@163.com'
+    MAIL_PASSWORD = 'VLEMBIFMETRSMLCJ'
+    MAIL_USE_SSL = True
+    MAIL_PORT = 465
+    MAIL_DEFAULT_SENDER = ('YSY', 'MAIL_USERNAME')
+
+class DevelopmentConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'hospital.db')
+
+class TestingConfig(BaseConfig):
+    TESTING =True
+    WTF_CSRF_ENABLED=False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'hospital.db')
+
+class ProductionConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'data.db')
+
+
+config= {
+        'development':DevelopmentConfig,
+        'testing':TestingConfig,
+        'production':ProductionConfig
+    }
